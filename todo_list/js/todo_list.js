63d93c6dfaa4fcdd393b2cf08f4132426735af41
RohEunChan <nong0324@naver.com>
2022-10-06 16:12:54 +0900

    [ing] todoList

---
+++ todo_list/js/todo_list.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById("new-task");
+const addButton = document.getElementsByTagName("button")[0];
+const incompleteTasksHoler = document.getElementById("incompleted-tasks");
+const completeTasksHoler = document.getElementById("completed-tasks");
+
