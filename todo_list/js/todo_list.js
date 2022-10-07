17b9458bf2acc709d0bf08406968c5a3eb56168e
RohEunChan <nong0324@naver.com>
2022-10-07 16:32:40 +0900

    [add] todoList - create const/let

--- todo_list/js/todo_list.js
+++ todo_list/js/todo_list.js
@@ -3,3 +3,11 @@ const addButton = document.getElementsByTagName("button")[0];
 const incompleteTasksHoler = document.getElementById("incompleted-tasks");
 const completeTasksHoler = document.getElementById("completed-tasks");
 
+const createNewTaskElement = function(taskString) {
+    let listItem = document.createElement("li");
+    let checkBox = document.createElement("input");
+    let label = document.createElement("label");
+    let editInput = document.createElement("input");
+    let editButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+}
