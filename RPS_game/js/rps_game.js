d54ded3e241e9aa938fda9e29a894c267d8c2734
RohEunChan <nong0324@naver.com>
2022-12-22 15:52:21 +0900

    [진행중] 가위바위보 게임 진행

---
+++ RPS_game/js/rps_game.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    // swiper
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 0,
+        slidesPerView: "auto",
+        freeMode: true,
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },        
+      });
+
+})
+
